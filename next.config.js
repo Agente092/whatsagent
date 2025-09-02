a16d362d1de3266e5dd516b5133f21f17f34c64b
GitHub API <noreply@github.com>
2025-09-02 10:32:35 +0200

    Agregar next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   
   // Variables de entorno públicas
   env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
   },
   
